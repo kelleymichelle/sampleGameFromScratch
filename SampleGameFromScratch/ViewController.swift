18f1e8bcd5a33b72be625797e6286e7691feb757
Kelley Chaplain <kmchaplain@gmail.com>
2020-06-02 15:12:40 -0500

    Initial Commit

---
+++ SampleGameFromScratch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleGameFromScratch
+//
+//  Created by Kelley Chaplain on 6/2/20.
+//  Copyright © 2020 Kelley Chaplain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
